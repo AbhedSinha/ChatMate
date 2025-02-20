d4896f64e26c67693b61b01a85314ff62b0f9b73
AbhedSinha <atggabaar133@gmail.com>
2025-02-21 02:31:39 +0530

    FinalCommit

---
+++ full-stack-flutter-app/client/lib/services/cache.dart
@@ -0,0 +1,21 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppCache {
+  static const _uidKey = "UID";
+
+  static SharedPreferences? _prefs;
+
+  static Future<void> init() async {
+    _prefs ??= await SharedPreferences.getInstance();
+  }
+
+  static int? get uid => _prefs?.getInt(_uidKey);
+
+  static void setUid(int id) {
+    _prefs?.setInt(_uidKey, id);
+  }
+
+  static void resetUid() {
+    _prefs?.remove(_uidKey);
+  }
+}
